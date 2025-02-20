b6ffdcc7b564348ba3c1cd4481f6a65c4c2bc178
Rainier Sarmiento <54691646+rensarmiento@users.noreply.github.com>
2025-02-20 01:19:33 -0800

    Start of Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import React from 'react';
 
 const App = () => {
-	return <h1>Welcome to proshop</h1>;
+	return (
+		<>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome to proshop</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
 };
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+				<Container>
+					{/* Sub component of Navbar */}
+					<Navbar.Brand href='/'>
+						<img src={logo} alt='proshop' />
+						ProShop
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ms-auto'>
+							<Nav.Link href='/cart'>
+								<FaShoppingCart />
+								Cart
+							</Nav.Link>
+
+							<Nav.Link href='/login'>
+								<FaUser />
+								Sign In
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+};
+
+export default Header;
